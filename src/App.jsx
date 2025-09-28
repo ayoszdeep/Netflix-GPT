b376503208708b3bfda351546718b7a315454ec3
ayoszdeep <ayoszdeepmishra2004@gmail.com>
2025-09-29 01:32:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+  
+      <div>
+      <h1 className='text-7xl text-red-500'> hello evryone</h1>
+       </div>
+          
+    
+  )
+}
+
+export default App
